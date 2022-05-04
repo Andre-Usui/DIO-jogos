6dc27a4d32fc155e49004bb856de7777c523280a
Andre-Usui <prof.andreusui@gmail.com>
2022-05-04 01:11:40 -0300

    adicionando o superpulo e recomeçar no Dino

--- Jogo Dino/script.js
+++ Jogo Dino/script.js
@@ -1,5 +1,6 @@
-const dino = document.querySelector('.dino');
+let dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+const rollando = document.querySelector('.rollando');
 
 
 let isJumping = false;
@@ -7,12 +8,20 @@ let isGameOver = false;
 let position = 0;
 
 function handleKeyDown(event) {
-  if (event.keyCode === 32) {
+  if (event.keyCode === 90) {
     if (!isJumping) {
       jump();
     }
   }
 }
+
+function handleKeyDown2(event) {
+    if (event.keyCode === 88) {
+      if (!isJumping) {
+        roll();
+      }
+    }
+  }
 
 function jump() {
   isJumping = true;
@@ -39,6 +48,33 @@ function jump() {
   }, 20);
 }
 
+function roll() {
+    isJumping = true;
+  
+    let upInterval = setInterval(() => {
+      if (position >= 200) {
+        // Descendo
+        clearInterval(upInterval);
+  
+        let downInterval = setInterval(() => {
+          if (position <= 0) {
+            clearInterval(downInterval);
+            isJumping = false;
+            dino.classList.remove('rollando');
+          } else {
+            position -= 15;
+            dino.style.bottom = position + 'px';
+          }
+        }, 30);
+      } else {
+        // Subindo
+        position += 15;
+        dino.classList.add('rollando');
+        dino.style.bottom = position + 'px';
+      }
+    }, 30);
+  }
+
 
 
 function createCactus() {
@@ -61,7 +97,8 @@ function createCactus() {
       // Game over
       clearInterval(leftTimer);
       isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+      document.body.innerHTML = 
+      ('<h1 class="game-over">Fim de jogo</h1> <button onclick=location.reload(true)> Repetir </button>');
 
     } else {
       cactusPosition -= 10;
@@ -74,3 +111,4 @@ function createCactus() {
 
 createCactus();
 document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keydown', handleKeyDown2);
